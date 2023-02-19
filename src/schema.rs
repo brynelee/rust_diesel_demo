fab38d2638472692e9508ccbc7abb047d88015f0
Bryne Lee <bryne_lxd@sina.com>
2023-02-19 12:49:31 +0800

    updated .env to support docker (xdpostgres for PG database address)

--- src/schema.rs
+++ src/schema.rs
@@ -1,6 +1,17 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    employees (id) {
+        id -> Int4,
+        first_name -> Varchar,
+        last_name -> Varchar,
+        department -> Varchar,
+        salary -> Int4,
+        age -> Int4,
+    }
+}
+
+diesel::table! {
     posts (id) {
         id -> Int4,
         title -> Varchar,
@@ -8,3 +19,8 @@ diesel::table! {
         published -> Bool,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    employees,
+    posts,
+);
